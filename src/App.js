d3fe103725c32e4d12f19be2eb50c813f98fc987
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-08-21 15:03:14 +0500

    double api call resolved

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, useParams } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useParams,
+} from "react-router-dom";
 import Chat from "./pages/chat/Chat.jsx";
 import OtpPage from "./pages/OTP/OtpPage.jsx";
 import "./App.css";
+import Admin from "./pages/chat/Admin.jsx";
 
-function ProtectedChat({ isAuthenticated,onLogout }) {
+function ProtectedChat({ isAuthenticated, onLogout }) {
   const { ticket_id } = useParams();
 
   if (!isAuthenticated) {
     return <Navigate to={`/authentication/${ticket_id}`} replace />;
   }
 
-  return <Chat onLogout={onLogout}/>;
+  return <Chat onLogout={onLogout} />;
 }
 
 const App = ({ isAuthenticated, onLogout, onLoginSuccess }) => {
   return (
-    
-      <div className="bg-wrapper">
-        <Routes>
-          {/* protected route */}
-          <Route
-            path="/ticket/:ticket_id"
-            element={<ProtectedChat isAuthenticated={isAuthenticated} onLogout={onLogout}  />}
-          />
-
-          {/* OTP auth route */}
-          <Route
-            path="/authentication/:ticket_id"
-            element={<OtpPage onLoginSuccess={onLoginSuccess}  />}
-          />
-        </Routes>
-      </div>
-   
+    <div className="bg-wrapper">
+      <Routes>
+        <Route
+          path="/ticket/:ticket_id"
+          element={
+            <ProtectedChat
+              isAuthenticated={isAuthenticated}
+              onLogout={onLogout}
+            />
+          }
+        />
+        <Route path="/ticketviewbyadmin/:ticket_id" element={<Admin />} />
+        <Route
+          path="/authentication/:ticket_id"
+          element={<OtpPage onLoginSuccess={onLoginSuccess} />}
+        />
+      </Routes>
+    </div>
   );
 };
 
